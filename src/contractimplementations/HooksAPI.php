f5521b24344b557893d917d997704198bb62ef92
Leo <losoviz@gmail.com>
2019-05-05 23:04:31 +0800

    Fixed PHPCS style

--- src/contractimplementations/HooksAPI.php
+++ src/contractimplementations/HooksAPI.php
@@ -3,23 +3,28 @@ namespace PoP\HooksWP\ContractImplementations;
 
 class HooksAPI implements \PoP\Hooks\Contracts\HooksAPIInterface
 {
-    public function addFilter($tag, $function_to_add, $priority = 10, $accepted_args = 1) {
+    public function addFilter($tag, $function_to_add, $priority = 10, $accepted_args = 1)
+    {
         return add_filter($tag, $function_to_add, $priority, $accepted_args);
     }
-    public function removeFilter($tag, $function_to_remove, $priority = 10) {
-    	return remove_filter($tag, $function_to_remove, $priority);
+    public function removeFilter($tag, $function_to_remove, $priority = 10)
+    {
+        return remove_filter($tag, $function_to_remove, $priority);
     }
-    public function applyFilters($tag, $value, ...$args) {
-    	return apply_filters($tag, $value, ...$args);
+    public function applyFilters($tag, $value, ...$args)
+    {
+        return apply_filters($tag, $value, ...$args);
     }
-    public function addAction($tag, $function_to_add, $priority = 10, $accepted_args = 1) {
-    	return add_action($tag, $function_to_add, $priority, $accepted_args);
+    public function addAction($tag, $function_to_add, $priority = 10, $accepted_args = 1)
+    {
+        return add_action($tag, $function_to_add, $priority, $accepted_args);
     }
-    public function removeAction($tag, $function_to_remove, $priority = 10) {
-    	return remove_action($tag, $function_to_remove, $priority);
+    public function removeAction($tag, $function_to_remove, $priority = 10)
+    {
+        return remove_action($tag, $function_to_remove, $priority);
     }
-    public function doAction($tag, ...$args) {
-    	return do_action($tag, ...$args);
+    public function doAction($tag, ...$args)
+    {
+        return do_action($tag, ...$args);
     }
 }
-

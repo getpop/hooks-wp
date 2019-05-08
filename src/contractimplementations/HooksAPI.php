8bdb071323a9c042808f724498bee2cbd2e942f5
Leo <losoviz@gmail.com>
2019-05-08 17:07:00 +0800

    Fixed namespace bug

--- src/contractimplementations/HooksAPI.php
+++ src/contractimplementations/HooksAPI.php
@@ -5,26 +5,26 @@ class HooksAPI implements \PoP\Hooks\Contracts\HooksAPIInterface
 {
     public function addFilter(string $tag, $function_to_add, int $priority = 10, int $accepted_args = 1): void
     {
-        add_filter($tag, $function_to_add, $priority, $accepted_args);
+        \add_filter($tag, $function_to_add, $priority, $accepted_args);
     }
     public function removeFilter(string $tag, $function_to_remove, int $priority = 10): bool
     {
-        return remove_filter($tag, $function_to_remove, $priority);
+        return \remove_filter($tag, $function_to_remove, $priority);
     }
     public function applyFilters(string $tag, $value, ...$args)
     {
-        return apply_filters($tag, $value, ...$args);
+        return \apply_filters($tag, $value, ...$args);
     }
     public function addAction(string $tag, $function_to_add, int $priority = 10, int $accepted_args = 1): void
     {
-        add_action($tag, $function_to_add, $priority, $accepted_args);
+        \add_action($tag, $function_to_add, $priority, $accepted_args);
     }
     public function removeAction(string $tag, $function_to_remove, int $priority = 10): bool
     {
-        return remove_action($tag, $function_to_remove, $priority);
+        return \remove_action($tag, $function_to_remove, $priority);
     }
     public function doAction(string $tag, ...$args): void
     {
-        do_action($tag, ...$args);
+        \do_action($tag, ...$args);
     }
 }
